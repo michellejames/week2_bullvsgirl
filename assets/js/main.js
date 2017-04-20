72596411d8b848c34093de6a8b87abdaf3bdb5d0
michellejames <shellyjames90@yahoo.com>
2017-04-20 17:28:57 -0400

    Bull Vs. Girl

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+console.log("Hello World from main.js!");
+
+$(function () {
+	$(".bull").hide();
+	$(".girl").hide();
+	$(".bullvsgirl").hide().fadeIn("slow");
+
+	$("#bull-button").on('click toggle', function () {
+		$(".bullvsgirl, h1, .girl").hide();
+		$(".bull").show();
+	});
+
+	$("#girl-button").on('click', function () {
+		$(this).empty().html("Back");
+		$(".bullvsgirl, h1, .bull").hide();
+		$(".girl").show();
+	});
+});
